2f0971c20b788b4763cc94a4d9280325f9ee90b7
kishibe-rohan <devrohan2000@gmail.com>
2022-03-22 19:22:16 +0530

    added dummy data

---
+++ data.js
@@ -0,0 +1,42 @@
+export const stories = [
+  {
+    id: 1,
+    pic: "https://pbs.twimg.com/profile_images/1241961365725540353/s1_Jg8dJ_400x400.jpg",
+    name: "stevensuptic",
+  },
+  {
+    id: 2,
+    pic: "https://pbs.twimg.com/profile_images/1084342761644572672/WTytSpjs_400x400.jpg",
+    name: "javascriptmastery",
+  },
+  {
+    id: 3,
+    pic: "https://pbs.twimg.com/card_img/1504578590708363264/aMka_0U6?format=jpg&name=large",
+    name: "notcib",
+  },
+  {
+    id: 4,
+    pic: "https://pbs.twimg.com/profile_images/1457497505298403328/J9-2jjjF_400x400.jpg",
+    name: "disguisedtoast",
+  },
+  {
+    id: 5,
+    pic: "https://pbs.twimg.com/media/ETMQuvRU0AABkm_.jpg:large",
+    name: "valkyrae",
+  },
+  {
+    id: 6,
+    pic: "https://pbs.twimg.com/profile_images/1237741639193571328/LryLEBqW_400x400.jpg",
+    name: "codercoder",
+  },
+  {
+    id: 7,
+    pic: "https://pbs.twimg.com/media/E4LFuuzVcAUIY4D?format=jpg&name=large",
+    name: "jesssquats",
+  },
+  {
+    id: 8,
+    pic: "https://pbs.twimg.com/media/EFQzeV7XsAAv4tF.jpg",
+    name: "jonmoxley",
+  },
+];
