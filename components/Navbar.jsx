0a79953cad89fea575915717ece5691fdf426bdd
kishibe-rohan <devrohan2000@gmail.com>
2022-03-23 19:59:08 +0530

    delete redundant files

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,13 +4,12 @@ import Image from 'next/image'
 import {SearchIcon,PlusCircleIcon,UserGroupIcon,HeartIcon,PaperAirplaneIcon,MenuIcon} from '@heroicons/react/outline'
 import { HomeIcon } from '@heroicons/react/solid'
 import { useSession,signIn, signOut } from 'next-auth/react'
-import { useRecoilState } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
+
 
 const Navbar = () => {
 
     const {data:session} = useSession();
-    const [open,setOpen] = useRecoilState(modalState);
+   
 
   return (
     <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
@@ -44,7 +43,7 @@ const Navbar = () => {
                 <PaperAirplaneIcon className='navBtn rotate-45'/>
                 <div className='absolute -top-1 -right-2 text-xs w-5 h-5 text-white bg-red-500 rounded-full flex items-center justify-center animate-pulse'>3</div>
                 </div>
-                <PlusCircleIcon className="navBtn" onClick={() => setOpen(true)}/>
+                <PlusCircleIcon className="navBtn"/>
                 <UserGroupIcon className='navBtn'/>
                 <HeartIcon className='navBtn'/>
 
