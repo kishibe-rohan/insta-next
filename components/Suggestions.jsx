0ec271ba86ffa633ae42d41bee3d5e9e05932dc0
kishibe-rohan <devrohan2000@gmail.com>
2022-03-23 08:49:27 +0530

    added suggestions panel

--- components/Suggestions.jsx
+++ components/Suggestions.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { stories } from '../data';
 
 const Suggestions = () => {
+
   return (
-    <div>
-      Suggestions 
+    <div className="mt-4 ml-10">
+      <div className='flex justify-between text-sm mb-5'>
+          <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+          <button className='text-gray-600 font-semibold'>See All</button>
+      </div>
+      {
+          stories.slice(3,8).map(profile => (
+              <div key={profile.id} className="flex items-center justify-between mt-3">
+                  <img className="w-10 h-10 rounded-full border p-[2px]"src={profile.pic}/>
+                  <div className="flex-1 ml-4">
+                      <h2 className='font-semibold text-sm'>{profile.name}</h2>
+                      <h3 className='text-xs text-gray-400'>Works at {profile.company}</h3>
+                  </div>
+
+                  <button className='text-sm font-bold text-blue-400'>Follow</button>
+              </div>
+          ))
+      }
     </div>
   )
 }
